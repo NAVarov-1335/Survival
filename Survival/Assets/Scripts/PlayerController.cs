68a7e6891c08b3abf2c1a490e9181bc84c901b8c
NAVarov-1335 <lgh559@gmail.com>
2021-06-02 19:33:08 +0900

    Run Edit

--- Survival/Assets/Scripts/PlayerController.cs
+++ Survival/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     //상태
     private bool isRun = false;
     private bool isCrouch = false;
-    private bool isGround = false;
+    private bool isGround = true;
 
     //앉는 시간
     [SerializeField]
@@ -62,8 +62,8 @@ public class PlayerController : MonoBehaviour
     {
         IsGround();
         TryJump();
-        TryRun();
         TryCrouch();
+        TryRun();
         Move();
         CameraRotation();
         CharacterRotation();
@@ -99,7 +99,6 @@ public class PlayerController : MonoBehaviour
     //동작실행
     IEnumerator CrouchCoroutine()
     {
-
         float _posY = theCamera.transform.localPosition.y;
         int count = 0;
 
@@ -178,7 +177,7 @@ public class PlayerController : MonoBehaviour
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
