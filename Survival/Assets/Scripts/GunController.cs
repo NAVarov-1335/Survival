29ae5c2cefdbfc9239e4c92d28b875b6fb15ba10
NAVarov-1335 <lgh559@gmail.com>
2021-06-03 17:11:59 +0900

    CrossHair

--- Survival/Assets/Scripts/GunController.cs
+++ Survival/Assets/Scripts/GunController.cs
@@ -269,4 +269,9 @@ public class GunController : MonoBehaviour
         audioSource.clip = _clip;
         audioSource.Play();
     }
+
+    public Gun GetGun()
+    {
+        return currentGun;
+    }
 }
