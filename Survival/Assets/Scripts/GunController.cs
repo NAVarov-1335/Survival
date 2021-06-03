0423e872580da90f605413c5734fed22dc82c499
NAVarov-1335 <lgh559@gmail.com>
2021-06-03 19:17:41 +0900

    Crosshair

--- Survival/Assets/Scripts/GunController.cs
+++ Survival/Assets/Scripts/GunController.cs
@@ -33,16 +33,19 @@ public class GunController : MonoBehaviour
     // 필요한 컴포넌트
     [SerializeField]
     private Camera theCam;
+    private Crosshair theCrosshair;
 
 
     // 피격 이펙트.
     [SerializeField]
     private GameObject hit_effect_prefab;
 
+
     void Start()
     {
         originPos = Vector3.zero;
         audioSource = GetComponent<AudioSource>();
+        theCrosshair = FindObjectOfType<Crosshair>();
     }
 
 
@@ -92,6 +95,7 @@ public class GunController : MonoBehaviour
     // 발사 후 계산.
     private void Shoot()
     {
+        theCrosshair.FireAnimation();
         currentGun.currentBulletCount--;
         currentFireRate = currentGun.fireRate; // 연사 속도 재계산.
         PlaySE(currentGun.fire_Sound);
@@ -104,7 +108,11 @@ public class GunController : MonoBehaviour
 
     private void Hit()
     {
-        if (Physics.Raycast(theCam.transform.position, theCam.transform.forward, out hitInfo, currentGun.range))
+        if (Physics.Raycast(theCam.transform.position, theCam.transform.forward +
+            new Vector3(Random.Range(-theCrosshair.GetAccuracy() - currentGun.accuracy, theCrosshair.GetAccuracy() + currentGun.accuracy),
+                        Random.Range(-theCrosshair.GetAccuracy() - currentGun.accuracy, theCrosshair.GetAccuracy() + currentGun.accuracy),
+                        0)
+            , out hitInfo, currentGun.range))
         {
             GameObject clone = Instantiate(hit_effect_prefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
             Destroy(clone, 2f);
@@ -181,7 +189,7 @@ public class GunController : MonoBehaviour
     {
         isFineSightMode = !isFineSightMode;
         currentGun.anim.SetBool("FineSightMode", isFineSightMode);
-
+        theCrosshair.FineSightAnimation(isFineSightMode);
         if (isFineSightMode)
         {
             StopAllCoroutines();
@@ -270,8 +278,14 @@ public class GunController : MonoBehaviour
         audioSource.Play();
     }
 
+
     public Gun GetGun()
     {
         return currentGun;
+    }
+
+    public bool GetFineSightMode()
+    {
+        return isFineSightMode;
     }
 }
